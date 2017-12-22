3eb761478316778c49500ae9484ec430f5364aa5
tecle@126.com <tecle@126.com>
2017-12-22 18:24:29 +0800

    update web html

---
+++ src/ikanfan/web/playconfig.js
@@ -0,0 +1,12 @@
+var playConfig = {
+            setting: 1,
+            PrevWebPage: '/ac/3/1-3.html',
+            NextWebPage: '/ac/3/1-5.html',
+            playname: 'iqiyi',
+            pv: '839590200,e4ba2b588e063ee392f2614722a5b221',
+            id: '3',
+            pid: '4',
+            sid: '1',
+            ckUrl: 'https://www.ikanfan.cn/api/play.php?type=',
+            copyRight: '0'
+        };
