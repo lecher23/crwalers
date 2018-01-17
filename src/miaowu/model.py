7a5b75a33af005392e3f546446c2da1f1c91b201
tecle@126.com <tecle@126.com>
2018-01-17 13:48:58 +0800

    update miaowu crawler.

--- src/miaowu/model.py
+++ src/miaowu/model.py
@@ -2,7 +2,7 @@
 
 from __future__ import unicode_literals
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, CHAR, String, DateTime
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -17,16 +17,37 @@ class Comic(Base):
     outer_id = Column(Integer, unique=True, nullable=False, comment='动漫外部ID, 指在被爬的网站的ID')
     name = Column(String(32), nullable=False, comment='动漫标题')
     info = Column(String(128), default='', comment='动漫简介')
-    detail = Column(String(512), default='', comment='动漫详情')
+    detail = Column(String(1024), default='', comment='动漫详情')
     score = Column(Integer, default=0, comment='动漫评分, 百分制')
-    img = Column(String(128), default='')
+    cover = Column(String(256), default='')
+    weight = Column(Integer, default=0, comment='动漫权重, 用来进行推荐排序')
+    year = Column(Integer, default=0, comment='出版年份')
+
+
+class CategoryDefine(Base):
+    __tablename__ = 'category_define'
+    cate_id = Column(Integer, primary_key=True, autoincrement=True, comment='分类ID')
+    cate_name = Column(String(16), unique=True, nullable=False, comment='分类名称')
+
+
+class Rank(Base):
+    __tablename__ = 'rank'
+    id = Column(Integer, primary_key=True, comment='排名的名次')
+    comic_id = Column(Integer, comment='动漫ID')
 
 
 class ComicTag(Base):
     __tablename__ = 'comic_tag'
     id = Column(Integer, primary_key=True, autoincrement=True)
     comic_id = Column(Integer, nullable=False)
-    tag_id = Column(Integer, nullable=False, comment='动漫标签ID')
+    tag_name = Column(String(16), nullable=False, comment='动漫标签')
+
+
+class ComicCategory(Base):
+    __tablename__ = 'comic_category'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    comic_id = Column(Integer, nullable=False)
+    cate_id = Column(Integer, nullable=False, comment='动漫分类')
 
 
 class ComicVideo(Base):
@@ -43,8 +64,4 @@ if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://licheng:Cg123456@192.168.0.253:3306/dongman?charset=utf8')
     SessionCls = sessionmaker(bind=engine)
     session = SessionCls()
-    try:
-        ret = engine.execute('drop table {};'.format(Comic.__tablename__))
-    except:
-        pass
     Base.metadata.create_all(engine)
